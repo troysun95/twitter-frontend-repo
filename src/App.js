343da8be8bcae6441b88d5c3c2b963eae339a915
troysun95 <sun0531158@gmail.com>
2023-10-30 20:44:40 +0800

    initailize repo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <>
+      <div className={styles.appContainer}>
+        <span className={styles.intro}>hello!
+        </span>
+      </div>
+     <span className={styles.intro}>hello!
+     </span>
+    </>
+    
+  );
+}
+
+export default App;
