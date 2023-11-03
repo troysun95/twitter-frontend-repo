23f65aad325a357badd669d889a0312a52db7e8e
MiJouHsieh <mijouhsieh@gmail.com>
2023-11-04 00:54:09 +0800

    docs admin: add fake data

---
+++ src/data/adminUsersData.js
@@ -0,0 +1,180 @@
+export const adminUsersData = [
+  {
+    id: '1',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.55555555k",
+    likesNumber: "211111110k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+   {
+    id: '2',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+   {
+    id: '3',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '4',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '5',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '6',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '7',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '8',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '9',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '10',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '11',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '12',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '13',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '14',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '15',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+  {
+    id: '16',
+    backgroundImg: 'https://picsum.photos/300/300?text=22',
+    img: 'https://picsum.photos/300/300?text=2',
+    account: 'STARBUCKS',
+    tagAccount: '@starbucks',
+    tweetsNumber: "1.5k",
+    likesNumber: "20k",
+    followingNumber: 34,
+    followersNumber: 59
+  },
+]
+
+
