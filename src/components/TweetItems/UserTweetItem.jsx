2ab5a292acd78c88b1d9f76ded8fb284657feb19
MiJouHsieh <mijouhsieh@gmail.com>
2023-11-08 21:40:19 +0800

    feat UserPage: 串接 user profile and tweets

---
+++ src/components/TweetItems/UserTweetItem.jsx
@@ -0,0 +1,58 @@
+import { ReactComponent as LikeIcon } from "icons/like.svg";
+import { ReactComponent as LikeActiveIcon } from "icons/likeActive.svg";
+
+import { ReactComponent as CommentIcon } from "icons/comment.svg";
+import styles4 from "styles/tweetsCollection.module.scss";
+
+const UserTweetItem = ({
+  userAvatar,userName,
+  userAccount,
+  account,
+  createdAt,
+  description,
+  isLiked,
+  likedUsersCount,
+  repliesCount,
+  updatedAt,
+}) => {
+  return (
+    <div className={styles4.tweetItem}>
+      <div className={styles4.avatar}>
+        <img src={userAvatar} aria-label={account} />
+      </div>
+      <div>
+        {/* 帳號 */}
+        <div className={styles4.accountTitle}>
+          <p className={styles4.accountName}>{userName}</p>
+          <p className={styles4.subTitle}>
+            <span>@{userAccount}</span>
+            <span>・{createdAt}</span>
+          </p>
+        </div>
+        <div className={styles4.descriptionContainer}>
+          <p>{description}</p>
+        </div>
+        <div className={styles4.interactionContainer}>
+          <p className={styles4.leftIcon}>
+            <span className={styles4.iconContainer}>
+              <CommentIcon className={styles4.icon} />
+            </span>
+            {repliesCount}
+          </p>
+          <p>
+            <span className={styles4.iconContainer}>
+              {isLiked ? (
+                <LikeIcon className={styles4.icon} />
+              ) : (
+                <LikeActiveIcon className={styles4.icon} />
+              )}
+            </span>
+            {likedUsersCount}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserTweetItem;
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,22 +1,88 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
-import MainNavbar from "components/MainNavbar";
-import NavItem from "components/NavItem";
 import { ReactComponent as HomeIcon } from "icons/home.svg";
 import { ReactComponent as UserActiveIcon } from "icons/userActive.svg";
+import { ReactComponent as EditProfileBtnIcon } from "icons/editProfileBtn.svg";
+import MainNavbar from "components/MainNavbar";
+import NavItem from "components/NavItem";
+import SwitchButtonPanel from "components/SwitchButtonPanel";
+import UserTweetItem from "components/TweetItems/UserTweetItem";
 
+import styles from "styles/UserSelfPage.module.scss";
 import styles3 from "styles/Layout3.module.scss";
+import styles4 from "styles/tweetsCollection.module.scss";
 
 import { getUserTweets } from "../api/twitter.js";
 // import { useNavigate } from "react-router-dom";
 // import { checkPermission } from "../api/auth";
 
+const TweetsCollection = ({ tweets, userInfo }) => {
+  return (
+    <div className={styles4.tweetsCollection}>
+      {tweets.map((userTweetItem) => {
+        return (
+          <UserTweetItem
+            key={userTweetItem.id}
+            {...userTweetItem}
+            userAvatar={userInfo.avatar}
+            userName={userInfo.name}
+            userAccount={userInfo.account}
+          />
+        );
+      })}
+    </div>
+  );
+};
+const UserProfile = ({ userInfo }) => {
+  return (
+    <div className={styles.userProfileContainer}>
+      <div className={styles.userProfileBg}>
+        <img src={userInfo.cover} aria-label={userInfo.account} />
+      </div>
+      <div className={styles.avatarNameContainer}>
+        <div className={styles.avatar}>
+          <img src={userInfo.avatar} aria-label={userInfo.account} />
+        </div>
+        <div className={styles.accountTitle}>
+          <p className={styles.accountName}>{userInfo.name}</p>
+          <p className={styles.subTitle}>{userInfo.account}</p>
+          <p className={styles.bio}>{userInfo.introduction}</p>
+          <div className={styles.trackingStatus}>
+            <p className={styles.followingNum}>
+              {userInfo.followingNum}個追隨中
+            </p>
+            <p>{userInfo.followerNum}位追隨者</p>
+          </div>
+        </div>
+      </div>
+      <button className={styles.editProfileBtn}>
+        <EditProfileBtnIcon />
+      </button>
+    </div>
+  );
+};
 
+const UserContent = ({ tweets, userInfo }) => {
+  return (
+    <div className={styles3.content}>
+      <UserProfile userInfo={userInfo} />
+      <SwitchButtonPanel />
+
+      <TweetsCollection tweets={tweets} userInfo={userInfo}>
+        <UserTweetItem userInfo={userInfo} />
+      </TweetsCollection>
+    </div>
+  );
+};
 const UserPage = () => {
   // const navigate = useNavigate();
   const savedUserInfo = JSON.parse(localStorage.getItem("user"));
   console.log("savedUserInfo", savedUserInfo);
   const id = savedUserInfo.id;
+
+  // const [userContent, setUserContent] = useState('tweets')
+  const [tweets, setTweets] = useState([]); //user發文
+  //const [replies, setReplies] = useState([]); //user回覆
 
   useEffect(() => {
     // 瀏覽使用者的推文
@@ -25,14 +91,14 @@ const UserPage = () => {
         const tweets = await getUserTweets(id);
         // 確認是否有tweets
         if (tweets) {
-        //   setTweets(tweets.map((tweet) => ({ ...tweet })));
+          setTweets(tweets.map((tweet) => ({ ...tweet })));
           console.log("tweets", tweets);
-        } 
+        }
         // else {
         //   setTweets(null);
         // }
       } catch (error) {
-        console.log('error', error);
+        console.error("error", error);
       }
     };
     getUserTweetsAsync();
@@ -74,6 +140,12 @@ const UserPage = () => {
           </NavItem>
         </MainNavbar>
       </div>
+      <UserContent
+        // userContent={userContent}
+        // onClick={handleClick}
+        userInfo={savedUserInfo}
+        tweets={tweets}
+      />
     </div>
   );
 };
