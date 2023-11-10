cd91149be1a8c83de9ab6d89de6a5d04ce5674e6
MiJouHsieh <mijouhsieh@gmail.com>
2023-11-10 13:00:21 +0800

    style: 自動排版

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,11 @@
-// import { useState } from "react";
-import { useState, useEffect } from "react";//add
-import {Login, checkPermission} from '../api/auth'
+import { useState } from "react";
 
 import styles from "styles/AdminPage.module.scss";
 import { ReactComponent as AdminLogInBtn } from "icons/adminLoginBtn.svg";
 import { ReactComponent as Logo } from "icons/logo.svg";
-// import {Login} from '../api/auth'
-import Swal from 'sweetalert2';
-import { useNavigate } from "react-router-dom"; 
+import {Login} from '../api/auth'
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const AuthInput = ({ type, label, value, placeholder, onChange }) => {
   return (
@@ -15,8 +13,8 @@ const AuthInput = ({ type, label, value, placeholder, onChange }) => {
       <label>{label}</label>
       <input
         type={type || "text"}
-        value={value || ""} 
-        placeholder={placeholder || ""} 
+        value={value || ""}
+        placeholder={placeholder || ""}
         onChange={(event) => onChange?.(event.target.value)}
       />
     </div>
@@ -26,27 +24,27 @@ const AuthInput = ({ type, label, value, placeholder, onChange }) => {
 const LoginPage = () => {
   const [account, setAccount] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate('')
+  const navigate = useNavigate('');
 
   //handler
   const handleClick = async () => {
     if (account.length === 0) {
-        return;
+      return;
     }
     if (password.length === 0) {
-        return;
+      return;
     }
     const data = await Login({
-    account,
-    password
+      account,
+      password,
     });
 
-    if(data.success){
+    if (data.success) {
       localStorage.setItem("authToken", data.token);
       //localStorage.setItem('Authorization', JSON.stringify(Authorization));
       //將 userid儲存進去，以幫助跳轉使用者時使用
       // localStorage.setItem('user', data.user)
-      localStorage.setItem("user", JSON.stringify(data.user)) //try
+      localStorage.setItem("user", JSON.stringify(data.user)); //try
 
       Swal.fire({
         position: "top",
@@ -55,38 +53,22 @@ const LoginPage = () => {
         icon: "success",
         showConfirmButton: false,
       });
-      navigate('/main')
-    }else{
-        Swal.fire({
-            position: 'top',
-            title: '登入失敗！',
-            timer: 1000,
-            icon: 'error',
-            showConfirmButton: false,
-        });
-      }
-    };
+      navigate("/main");
+    } else {
+      Swal.fire({
+        position: "top",
+        title: "登入失敗！",
+        timer: 1000,
+        icon: "error",
+        showConfirmButton: false,
+      });
+    }
+  };
 
-  useEffect(() => {
-    //add
-    const checkTokenIsValid = async () => {
-      const authToken = localStorage.getItem("authToken");
-      if (!authToken) {
-        return;
-      }
-      const result = await checkPermission(authToken);
-
-      if (result) {
-        navigate('/main');
-      }
-    };
-    checkTokenIsValid();
-  }, [navigate]);
-  
   return (
     <div className={styles.adminLogInContainer}>
       <div className={styles.brandContainer}>
-        <Logo/>
+        <Logo />
       </div>
       <h3>登入 Alphitter</h3>
       <div>
@@ -104,13 +86,26 @@ const LoginPage = () => {
           placeholder={"請輸入密碼"}
           onChange={(passwordInputValue) => setPassword(passwordInputValue)}
         />
-
       </div>
       <button>
-        <AdminLogInBtn  onClick={handleClick}/>
+        <AdminLogInBtn onClick={handleClick} />
       </button>
-      <div className={styles.linkText} onClick={()=>{navigate('/register')}}>註冊</div>
-      <div className={styles.linkText} onClick={()=>{navigate('/admin')}}>後台登入</div>
+      <div
+        className={styles.linkText}
+        onClick={() => {
+          navigate("/register");
+        }}
+      >
+        註冊
+      </div>
+      <div
+        className={styles.linkText}
+        onClick={() => {
+          navigate("/admin");
+        }}
+      >
+        後台登入
+      </div>
     </div>
   );
 };
