5b9d67293ac68c9d7a5bbd2f0080748f21e12082
MiJouHsieh <mijouhsieh@gmail.com>
2023-11-10 16:12:37 +0800

    fix: SwitchButtonPanel 切換內容時，btn 底線一起切換 style: 移除不必要code

--- src/components/TrackingSwitchPanel.jsx
+++ src/components/TrackingSwitchPanel.jsx
@@ -1,40 +1,49 @@
-import clsx from 'clsx';
-import styles from "styles/SwitchButtonPanel.module.scss"
-import { useState } from 'react';
-import { useNavigate } from "react-router-dom"
+import clsx from "clsx";
+import styles from "styles/SwitchButtonPanel.module.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function SwitchButton ({title, isActive, id, handelClick}){
-    return (
-        <div className={clsx(styles.button, {[styles.active]: isActive})} onClick={() => handelClick(id)} id={id}>
-        <span>{title}</span>
-        </div>
-    )
+function SwitchButton({ title, isActive, id, handelClick }) {
+  return (
+    <div
+      className={clsx(styles.button, { [styles.active]: isActive })}
+      onClick={() => handelClick(id)}
+      id={id}
+    >
+      <span>{title}</span>
+    </div>
+  );
 }
 
 export default function SwitchButtonPanel() {
-    //state
-    const prevActive = [0, 1 ]
-    const [isActive, setIsActive] = useState(prevActive)
-    const navigate = useNavigate('')
+  //state
+  const [isActive, setIsActive] = useState(0);
+  const navigate = useNavigate("");
 
-    //handler
-    const handelClick = (id) => {
-        if(id === 0){
-            setIsActive([1, 0])
-            // alert("GET follower")
-            navigate('/user/follower')
-        }else if(id === 1){
-            setIsActive([0, 1])
-            // alert("GET following")
-            navigate('/user/following')
-
-        }
+  //handler
+  const handelClick = (id) => {
+    setIsActive(id);
+    if (id === 0) {
+      navigate("/user/follower");
+    } else if (id === 1) {
+      navigate("/user/following");
     }
+  };
 
-    return(
-        <div className={styles.buttonPanel}>
-            <SwitchButton title="追蹤者" isActive={isActive[0]} id={0} handelClick={handelClick}/>
-            <SwitchButton title="正在追蹤" isActive={isActive[1]} id={1} handelClick={handelClick}/>
-        </div>
-    )
+  return (
+    <div className={styles.buttonPanel}>
+      <SwitchButton
+        title="追蹤者"
+        isActive={isActive === 0}
+        id={0}
+        handelClick={handelClick}
+      />
+      <SwitchButton
+        title="正在追蹤"
+        isActive={isActive === 1}
+        id={1}
+        handelClick={handelClick}
+      />
+    </div>
+  );
 }
--- src/components/TweetItems/UserLikeItem.jsx
+++ src/components/TweetItems/UserLikeItem.jsx
@@ -2,7 +2,6 @@ import { ReactComponent as LikeActiveIcon } from "icons/likeActive.svg";
 import { ReactComponent as CommentIcon } from "icons/comment.svg";
 import styles4 from "styles/TweetsCollection.module.scss";
 
-
 const UserLikeItem = ({
   Tweet,
   repliesCount,
